9e178954597e16c6f007a4ff9b78c1c581c234cb
Samara Luiza Cunha Silva <samaralcunhas@gmail.com>
2023-06-29 11:42:27 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/header/index'
+
+function App() {
+  return (
+    <main className="bg-[#AED1E6]">
+      <Header/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/header/index.jsx
@@ -0,0 +1,36 @@
+import styles from './header.module.css'
+
+function Header() {
+    return (
+        <header className="flex items-center p-4 py-6 justify-between gap-8 bg-black/10">
+            <hr className="h-2 w-28 bg-[#CFE8EF] rounded" />
+
+            
+                <Title/>
+
+            <hr className="h-2 w-full bg-[#CFE8EF] rounded" />
+            <nav className="flex gap-8 text-lg text-[#0077A4]">  
+                <a className="hover:underline hover:text-[#1F4268]">Home</a>
+                <a className="hover:underline hover:text-[#1F4268]">Sobre</a>
+                <a className="hover:underline hover:text-[#1F4268]">Linguagens</a>
+                <a className="hover:underline hover:text-[#1F4268]">Projetos</a>
+                <a className="hover:underline hover:text-[#1F4268]">Contatos</a>
+            </nav>
+            <hr className="h-2 w-10 bg-[#CFE8EF] rounded" />
+        </header>
+    )
+}
+
+export default Header 
+
+
+function Title() {
+    return (
+        <div className='flex gap-2 items-center text-xl text-[#495867]'>
+            <p>{`{`}</p>
+            <p>Samara</p>
+            <p>Luiza</p>
+            <p>{`}`}</p>
+        </div>
+    )
+}
