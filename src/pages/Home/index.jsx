ce176497a286b5b60d0cfaba843923962d84baa3
samaralcunha <samaralcunhas@gmail.com>
2023-07-03 15:06:53 -0300

    coloquei container

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import Footer from '../../components/footer'
 import Header from '../../components/header'
+import Container from '../../components/container'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
@@ -7,19 +8,21 @@ export default function Home() {
         <>
             <main className="bg-[#BBDDF2] h-screen grid grid-rows-[auto_1fr_auto]">
                 <Header />
-                <section className='flex justify-around items-center font-roboto py-10'>
-                    <div className='flex flex-col'>
-                        <p className='text-xs font-semibold'>Olá meu nome é</p>
-                        <h1 className='text-2xl my-4'>Samara Luiza <br /> Cunha Silva</h1>
-                        <Link to='/sobre' className='rounded border-2 border-[#025A7B] p-1 text-sm 
+                <Container>
+                    <section className='flex justify-around items-center font-roboto py-10'>
+                        <div className='flex flex-col'>
+                            <p className='text-sm font-semibold'>Olá meu nome é</p>
+                            <h1 className='text-3xl my-4'>Samara Luiza <br /> Cunha Silva</h1>
+                            <Link to='/sobre' className='rounded border-2 border-[#025A7B] p-1 text-base text-center
                         hover:bg-[#025A7B] hover:text-white hover:scale-105 ease-in duration-300'>
-                            Saiba mais sobre mim
-                        </Link>
-                    </div>
-                    <figure >
-                        <img className='h-60' src="/undraw_blooming_re_2kc4.svg" />
-                    </figure>
-                </section>
+                                Saiba mais sobre mim
+                            </Link>
+                        </div>
+                        <figure >
+                            <img className='h-60' src="/undraw_blooming_re_2kc4.svg" />
+                        </figure>
+                    </section>
+                </Container>
                 <Footer />
             </main>
         </>
