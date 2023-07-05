d74f8efc3b1da82c54b0edfe15bd0c929dffb535
Samara Luiza Cunha Silva <samaralcunhas@gmail.com>
2023-07-05 11:36:42 -0300

    mudei nome arquivo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import Footer from '../../components/footer/footer'
-import Header from '../../components/header/header'
-import Container from '../../components/container/container'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Container from '../components/Container'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
