adc1dfe464c340e7ddb88e65f6ab0a55a2793861
Samara Luiza Cunha Silva <samaralcunhas@gmail.com>
2023-07-06 11:38:26 -0300

    criei Card e coloquei em Projetos
    card ainda esta vazio

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+export default function Card() {
+    return (
+        <section>
+            oi
+        </section>
+    )
+}
--- src/pages/Projetos.jsx
+++ src/pages/Projetos.jsx
@@ -1,13 +1,17 @@
 import Header from "../components/Header";
 import Container from "../components/Container";
 import Footer from "../components/Footer";
+import Card from "../components/Card"
 
 export default function Projetos() {
-    <>
-    <Header/>
-    <Container>
-
-    </Container>
-    <Footer/>
-    </>
+    return (
+        <>
+            <Header />
+            <Container>
+                {/* <h2>Projetos</h2> */}
+                <Card />
+            </Container>
+            <Footer />
+        </>
+    )
 }
