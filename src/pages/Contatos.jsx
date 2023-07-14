04ca043e30aba9f9262fd29b157279af4fa961ab
samaralcunha <samaralcunhas@gmail.com>
2023-07-14 15:23:25 -0300

    mudei mt coisa

--- src/pages/Contatos.jsx
+++ src/pages/Contatos.jsx
@@ -7,8 +7,19 @@ export default function Contatos() {
             <Header />
             <Container>
                 <section>
-                    <h1>Contatos</h1>
-                    <h2>Entre em contato :{')'}</h2>
+                    <h1 className='text-[#025A7B] text-lg'>Contatos</h1>
+                    <h2 className='text-[#0353A4] text-sm font-semibold'>Entre em contato :{')'}</h2>
+                    <div className='flex w-max gap-24 m-5'>
+                        <div>
+                            <h3 className='text-[#577399] font-bold'>Telefone</h3>
+                            <p className='text-[#0353A4]'>{'('}31{')'} 99674-9750</p>
+                        </div>
+
+                        <div>
+                            <h3 className='text-[#577399] font-bold'>E-mail</h3>
+                            <p className='text-[#0353A4]'>samaralcunhas@gmail.com</p>
+                        </div>
+                    </div>
                 </section>
             </Container>
             <Footer />
--- src/routes.jsx
+++ src/routes.jsx
@@ -4,18 +4,20 @@ import Sobre from './pages/Sobre'
 import Linguagens from './pages/Linguagens'
 import Projetos from './pages/Projetos'
 import Contatos from './pages/Contatos'
+import Page404 from './pages/Page404'
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />}></Route>
-                <Route path='/sobre' element={<Sobre />}></Route>
-                <Route path='/linguagens' element={<Linguagens />}></Route>
-                <Route path='/projetos' element={<Projetos />}></Route>
-                <Route path='/contatos' element={<Contatos />}></Route>
-                {/* <Route path='*' element={<Page404/>}></Route>  */}
-            </Routes>
+        
+                <Routes>
+                    <Route path='/' element={<Home />}></Route>
+                    <Route path='/sobre' element={<Sobre />}></Route>
+                    <Route path='/linguagens' element={<Linguagens />}></Route>
+                    <Route path='/projetos' element={<Projetos />}></Route>
+                    <Route path='/contatos' element={<Contatos />}></Route>
+                    <Route path='*' element={<Page404 />}></Route>
+                </Routes>
         </BrowserRouter>
     )
 }
